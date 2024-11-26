3cd2769c180a61cf661e3901983a6b556abbf77d
Anton Arzamastsev <tonich@sbor.net>
2024-11-26 18:40:02 +0300

    refactor: rewrite capitalize.test using the assert module

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,9 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// при использовании strict-режима
+// проверка equal равносильна strictEqual
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
